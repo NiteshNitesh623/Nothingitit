6d10d571fb533d5c78e43d424d5e3aa7ffda0eb2
NiteshNitesh623 <niteshkanade228@gmail.com>
2024-10-13 12:52:17 +0530

    add cpp file

---
+++ mycodecpp.cpp
@@ -0,0 +1,13 @@
+/* # this code gone in githuk */
+#include<iostream>
+
+int main(int argc, char **argv){
+	std::cout << "this code is meant to be pushed on github" << std::endl;
+	int origin = 1;
+	if (origin == 1){
+		std::cout << "you are on main branch" <<  std::endl;
+		}
+	else
+		std::cout << "you are distracted" << std::endl;
+	return 0;
+}
